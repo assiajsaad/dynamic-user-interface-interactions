3ce2da10b355b10fc6fcdb9ceaca580ddfcf9e86
assiajsaad <assia.j.saad@gmail.com>
2024-04-11 16:38:59 +0300

    Fixed carousel image loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import "./style.css";
 import { dropdown } from "./dropdown";
 import { carousel } from "./carousel";
+// eslint-disable-next-line no-unused-vars
+// import Dogs from "./assets/images/dogs.jpg";
+// // eslint-disable-next-line no-unused-vars
+// import Colors from "./assets/images/colors.jpg";
+// // eslint-disable-next-line no-unused-vars
+// import Bees from "./assets/images/bees.jpg";
 
 dropdown();
 
